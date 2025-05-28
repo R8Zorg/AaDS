6005076a98669b7f859c0f907dfa28200a7c1c27
Hak1e <hakie.notiko@gmail.com>
2025-05-28 18:34:45 +0400

    Little update

--- labs/lab_06/main.py
+++ labs/lab_06/main.py
@@ -38,53 +38,29 @@ def func_measure_time(func, n):
     return end_time - start_time, result
 
 
-def comparing():
+def comparing(num):
     print("n\tВремя рекурсивного алгоритма\tВремя итерационного алгоритма")
     print("---------------------------------------------------------------------")
 
     rec_time, rec_value = 0, 0
     iter_time, iter_value = 0, 0
     try:
-        rec_time, rec_value = func_measure_time(f_rec, n)
+        rec_time, rec_value = func_measure_time(f_rec, num)
 
     except RecursionError:
-        print(f"{n}\tГлубина рекурсии превышена\t{iter_time * 1000:.6f}")
+        print(f"{num}\tГлубина рекурсии превышена\t{iter_time * 1000:.6f}")
 
     except Exception as e:
-        print(f"{n}\tОшибка: {str(e)}")
+        print(f"{num}\tОшибка: {str(e)}")
 
     try:
-        iter_time, iter_value = func_measure_time(f_iter, n)
+        iter_time, iter_value = func_measure_time(f_iter, num)
 
     except Exception as e:
-        print(f"{n}\tОшибка: {str(e)}")
+        print(f"{num}\tОшибка: {str(e)}")
 
-    print(f"{n}\t{rec_time * 1000:.6f}\t\t\t{iter_time * 1000:.6f}")
-
-
-def detemenite_borders():
-    print("\nОпределение границ применимости алгоритмов:")
-
-    big_num = 1000
-    max_rec_n = 0
-    for n in range(1, big_num):
-        try:
-            f_rec(n)
-            max_rec_n = n
-        except Exception:
-            print(f"Рекурсивный алгоритм: максимальное n = {max_rec_n}")
-            break
-
-    max_it_n = 0
-    for n in range(1, big_num):
-        try:
-            f_iter(n)
-            max_it_n = n
-        except Exception:
-            print(f"Итеративный алгоритм: алгоритм: максимальное n = {max_it_n}")
-            break
+    print(f"{num}\t{rec_time * 1000:.6f}\t\t\t{iter_time * 1000:.6f}")
 
 
 n = int(input("Введите n: "))
-comparing()
-detemenite_borders()
+comparing(n)
